5626769c71f8e97f3e018fc5e15943bf57ad3963
chenfeng <chenfeng@iastate.edu>
2016-09-09 14:12:57 -0500

    Modified Encryption Function

--- lab/lab1/ServerClientChat/src/chat/Test.java
+++ lab/lab1/ServerClientChat/src/chat/Test.java
@@ -1,32 +1,62 @@
 package chat;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Test {
 
-	public static void main(String[] args) throws UnsupportedEncodingException {
+	public static void main(String[] args) throws UnsupportedEncodingException, FileNotFoundException {
 		String msg = "hello world!";
+		DataOutputStream streamOut = new DataOutputStream(new FileOutputStream("res/file.txt"));
+		PrintWriter out = new PrintWriter(streamOut);
 		byte[] en = encrypMSG(msg);
-		String de = decrypMSG(en);
+		for (int i = 0; i < en.length; i++) {
+			out.print(en[i]);
+			out.print(" ");
+		}
+
+		out.println();
+		out.flush();
+
+		/////////////////////////////////////////////////////////////////////////////////////////////
+		Scanner in = new Scanner(new FileInputStream("res/file.txt"));
+		ArrayList<Byte> bf = new ArrayList<>();
+
+		while (in.hasNextInt()) {
+			int tempInt = in.nextInt();
+			byte tempByte = (byte) tempInt;
+			bf.add(tempByte);
+		}
+
+		String de = decrypMSG(bf);
 		System.out.println(de);
-		
-		
+
+		out.close();
+		in.close();
 	}
-	public static byte[] encrypMSG (String msg) throws UnsupportedEncodingException{
+
+	public static byte[] encrypMSG(String msg) throws UnsupportedEncodingException {
 		byte[] bf = msg.getBytes();
 		byte[] encrypBF = new byte[bf.length];
-		for(int i = 0; i< bf.length; i++){
+		for (int i = 0; i < bf.length; i++) {
 			encrypBF[i] = (byte) (bf[i] ^ 240);
-//			System.out.print(String.format("%x, ", encrypBF[i]));
+			// System.out.print(String.format("%x, ", encrypBF[i]));
 		}
-//		System.out.println();
+		// System.out.println();
 		return encrypBF;
 	}
-	
-	public static String decrypMSG (byte[] encrypBF) throws UnsupportedEncodingException{
-		byte[] decrypBF = new byte[encrypBF.length];
-		for(int i = 0; i< encrypBF.length; i++){
-			decrypBF[i] = (byte) (encrypBF[i] ^ 240);
+
+	public static String decrypMSG(ArrayList<Byte> encrypBF) throws UnsupportedEncodingException {
+		byte[] decrypBF = new byte[encrypBF.size()];
+		for (int i = 0; i < encrypBF.size(); i++) {
+			decrypBF[i] = (byte) (encrypBF.get(i) ^ 240);
 //			System.out.print(String.format("%x, ", decrypBF[i]));
 		}
 //		System.out.println();

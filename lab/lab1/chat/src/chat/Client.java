cb162dac6ab2229df9c38282c044f74e45c03126
chenfeng <chenfeng@iastate.edu>
2016-09-21 20:57:40 -0500

    Finished lab1 Chat Application

---
+++ lab/lab1/chat/src/chat/Client.java
@@ -0,0 +1,199 @@
+
+package chat;
+
+import java.net.*;
+import java.nio.file.Files;
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Client implements Runnable {
+
+	private String username;
+	private Socket serverSocket;
+	private DataOutputStream streamOut = null;
+	private Thread clientThread = null;
+	private Scanner in = new Scanner(System.in);
+
+	public Client() {
+		this.username = login();
+		try {
+			serverSocket = new Socket("localhost", 1222);
+			this.streamOut = new DataOutputStream(serverSocket.getOutputStream());
+			start();
+		} catch (UnknownHostException h) {
+			System.out.println("Unknown Host " + h.getMessage());
+			System.exit(1);
+		} catch (IOException e) {
+			System.out.println("IO exception: " + e.getMessage());
+			System.exit(1);
+		}
+	}
+
+	@Override
+	public void run() {
+
+		strEncryptionSend("usrnme" + username);
+
+		if (username.equals("admin")) {
+			System.out.printf(
+					"1. To broadcast, please enter \"broadcast\"\n2. To list messages so far, please enter \"list\"\n3. To delete message, please enter \"delete\"\n");
+		} else {
+			System.out.printf("To send an image, please enter \"image\".Otherwise, it is considered a message.\n");
+		}
+
+		while (true) {
+
+			if (username.equals("admin")) {
+				
+				while (!in.hasNextLine());
+				String msg = in.nextLine();
+
+				if (msg.equals("broadcast")) {
+					System.out.println("what do you want to broadcast?");
+					while (!in.hasNextLine());
+					String temp = in.nextLine();
+					strEncryptionSend("msg" + "admin" + ": " + temp);
+				}else if(msg.equals("list")){
+					strEncryptionSend("list" + "admin");
+				}else if(msg.equals("delete")){
+					System.out.println("which line do you want to delete?");
+					while (!in.hasNextLine());
+					String temp = in.nextLine();
+					strEncryptionSend("delete" + temp);
+				}else {
+					System.out.println("Please enter a valid input!");
+					System.out.println("You can enter \"broadcast\"");
+					System.out.println("You can enter \"list\"");
+					System.out.println("You can enter \"delete\"");
+				}
+			} else {
+
+				while (!in.hasNextLine());
+				String msg = in.nextLine();
+
+				if (msg.equals("image")) {
+					System.out.println("Please enter your image name: (i.e. happy.png)");
+					String imageName = in.nextLine();
+
+					// send image file to server
+					File f = new File("res/" + imageName);
+					imgEncryptionSend(f);
+				} else {
+					strEncryptionSend("msg" + username + ": " + msg);
+				}
+
+			}
+		} // End while(true)
+	}// End run()
+
+	public String login() {
+		System.out.print("Please enter your username: ");
+		return in.nextLine();
+	}
+
+	public void start() {
+		clientThread = new Thread(this);
+		clientThread.start();
+
+		Thread t2 = new Thread() {
+			@Override
+			public void run() {
+				while (true) {
+					Scanner in;
+					try {
+						in = new Scanner(new DataInputStream(serverSocket.getInputStream()));
+						while (in.hasNextLine()) {
+							String msg = in.nextLine();
+							if (msg.startsWith("image send: ")) {
+								BufferedImage img = ImageIO
+										.read(ImageIO.createImageInputStream(serverSocket.getInputStream()));
+								JFrame f = new JFrame();
+								f.getContentPane().add(new JLabel(new ImageIcon(img)));
+								f.pack();
+								f.setVisible(true);
+							}
+							System.out.println(msg);
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+		t2.start();
+	}
+
+	public synchronized void handleChat(String msg) {
+		PrintWriter out = new PrintWriter(streamOut);
+		out.println(msg);
+		out.flush();
+	}
+
+	private synchronized void strEncryptionSend(String msg) {
+		char key = 0xf0;
+		byte[] msgBytes = msg.getBytes();
+		for (byte b : msgBytes)
+			b ^= key;
+
+		try {
+			streamOut.write(msgBytes);
+			streamOut.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private synchronized void imgEncryptionSend(File f) {
+		byte[] bf;
+		try {
+			bf = Files.readAllBytes(f.toPath());
+			int r = bf.length % 3;
+			byte[] result;
+			if (r == 1) {
+				result = new byte[bf.length * 4 / 3 + 2];
+			} else if (r == 2) {
+				result = new byte[bf.length * 4 / 3 + 3];
+			} else {
+				result = new byte[bf.length * 4 / 3];
+			}
+
+
+			for (int i = 0; i < bf.length; i += 3) {
+				result[i / 3 * 4] = (byte) ((0xfc & bf[i]));
+				if (i / 3 * 4 + 1 < result.length)
+					result[i / 3 * 4 + 1] = (byte) ((0xc0 & (bf[i] << 6)) | (0x3c & (bf[i+1] >> 2)));
+				if (i / 3 * 4 + 2 < result.length)
+					result[i / 3 * 4 + 2] = (byte) ((0xf0 & (bf[i+1] << 4)) | (0x0c & (bf[i+2] >> 4)));
+				if (i / 3 * 4 + 3 < result.length)
+					result[i / 3 * 4 + 3] = (byte) ((0xfc & (bf[i+2] << 2)));
+			}
+			
+			if (bf.length % 3 == 2) {
+				result[result.length - 1] = 0x00;
+				result[result.length - 2] = 0x00;
+			} else if (bf.length % 3 == 1) {
+				result[result.length - 1] = 0x00;
+			}
+			streamOut.write(result);
+			streamOut.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+
+		Client c = new Client();
+
+	}
+
+}
---
+++ lab/lab1/chat/src/chat/Server.java
@@ -0,0 +1,227 @@
+package chat;
+
+import java.net.*;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+import java.awt.image.BufferedImage;
+import java.awt.print.PrinterAbortException;
+import java.io.*;
+
+public class Server implements Runnable {
+
+	private ArrayList<Socket> clientList;
+	private ArrayList<String> messages;
+	private ServerSocket serverSocket;
+	private Thread mainThread;
+	private Socket adminSocket = null;
+	private File file = new File("chat.txt");
+	private PrintWriter pw;
+
+	public Server() {
+
+		clientList = new ArrayList<Socket>();
+		try {
+			System.out.println("Binding to port " + 1222 + ", please wait  ...");
+			serverSocket = new ServerSocket(1222);
+			System.out.println("Server started: " + serverSocket);
+			pw = new PrintWriter(file);
+			messages = new ArrayList<String>();
+			start();
+		} catch (IOException ioe) {
+			System.out.println("Can not bind to port " + 1222 + ": " + ioe.getMessage());
+		}
+
+	}
+
+	@Override
+	public void run() {
+		Socket clientSocket = null;
+		while (true) {
+			try {
+				clientSocket = serverSocket.accept();
+				addThread(clientSocket);
+			} catch (IOException e) {
+				e.printStackTrace();
+				return;
+			}
+		}
+	}
+
+	private void addThread(Socket socket) throws IOException {
+
+		clientList.add(socket);
+
+		Thread t = new Thread() {
+			@Override
+			public void run() {
+				getMsg(socket);
+			}
+		};
+		t.start();
+	}
+
+	private void getMsg(Socket socket) {
+		String msg = "";
+		try {
+			BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
+			int len;
+			while (true) {
+				while ((len = bis.available()) > 0) {
+					byte bf[] = new byte[len];
+					bis.read(bf);
+					msg = strDecryption(bf);
+					handleMsg(msg, socket, bf);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private void handleMsg(String msg, Socket socket, byte[] bf) {
+		if (msg.startsWith("usrnme")) {
+			String username = msg.substring(6);
+			if (username.equals("admin")) {
+				adminSocket = socket;
+				System.out.println("Admin connected!");
+			} else {
+				System.out.println(username + " connected!");
+			}
+		} else if (msg.startsWith("msg")) {
+			// write msg to file
+			msg = msg.substring(3);
+			pw.println(msg);
+			pw.flush();
+
+			for (Socket s : clientList) {
+				if (s.equals(socket))
+					continue;
+
+				PrintWriter out;
+				try {
+					out = new PrintWriter(new DataOutputStream(s.getOutputStream()));
+					out.println(msg);
+					out.flush();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} // End for loop
+		} else if (msg.equals("listadmin")) {
+			// list messages from chat.txt
+			try {
+				BufferedReader listChat = new BufferedReader(new FileReader("chat.txt"));
+				PrintWriter out;
+				String line;
+				int lineNum = 0;
+				while ((line = listChat.readLine()) != null) {
+					out = new PrintWriter(new DataOutputStream(adminSocket.getOutputStream()));
+					out.println(lineNum + "> " + line);
+					lineNum = lineNum + 1;
+					out.flush();
+				}
+				listChat.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println("chat.txt is here:");
+		} else if (msg.startsWith("delete")) {
+			// delete a line from chat.txt
+			String lineNum = msg.substring(6);
+			try {
+				File in = new File("chat.txt");
+				File temp = new File(in.getAbsolutePath() + ".tmp");
+				BufferedReader br = new BufferedReader(new FileReader("chat.txt"));
+				PrintWriter tempPW = new PrintWriter(new FileWriter(temp));
+
+				String line;
+				int currentline = 0;
+				while ((line = br.readLine()) != null) {
+					if (!lineNum.equals(currentline + "")) {
+						tempPW.println(line);
+						tempPW.flush();
+					}
+					currentline = currentline + 1;
+				}
+				tempPW.close();
+				br.close();
+
+				// Delete old file
+				if (!in.delete())
+					System.out.println("delete old file");
+
+				// Rename new file
+				if (!temp.renameTo(in))
+					System.out.println("rename new file");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println("deleted line " + msg);
+		} else {
+			try {
+				BufferedImage img = imgDecryptionSend(bf);
+
+				for (Socket s : clientList) {
+					if (s.equals(socket))
+						continue;
+					PrintWriter out;
+					try {
+						out = new PrintWriter(new DataOutputStream(s.getOutputStream()));
+						out.println("image send: ");
+						out.flush();
+
+						ImageIO.write(img, "png", s.getOutputStream());
+						img.flush();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private String strDecryption(byte[] bf) throws UnsupportedEncodingException {
+		char key = 0xf0;
+		for (byte b : bf)
+			b ^= key;
+		return new String(bf, "UTF-8");
+	}
+
+	private BufferedImage imgDecryptionSend(byte[] bf) throws IOException, ClassNotFoundException {
+		byte[] result = new byte[bf.length / 4 * 3];
+
+		for (int i = 0; i < bf.length; i += 4) {
+			result[i / 4 * 3] = (byte) ((0xfc & bf[i]) | (0x03 & (bf[i + 1] >> 6)));
+			result[i / 4 * 3 + 1] = (byte) ((0xf0 & (bf[i + 1] << 2)) | (0x0f & (bf[i + 2] >> 4)));
+			result[i / 4 * 3 + 2] = (byte) ((0xc0 & (bf[i + 2] << 4)) | (0x3f & (bf[i + 3] >> 2)));
+		}
+
+		InputStream in = new ByteArrayInputStream(result);
+		BufferedImage img = ImageIO.read(in);
+		return img;
+
+	}
+
+	private void start() {
+		mainThread = new Thread(this);
+		mainThread.start();
+	}
+
+	public static void main(String[] args) {
+		Server s = new Server();
+	}
+
+}
